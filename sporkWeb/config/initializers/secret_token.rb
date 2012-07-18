648df1b91523924726a22a324b0f436c85d33e8e
blake <blake.rego@gmail.com>
2012-07-17 22:30:14 -0400

    changing the rails project. the previous one was not compatible with 1.9.2@rails-3.0.3

--- sporkWeb/config/initializers/secret_token.rb
+++ sporkWeb/config/initializers/secret_token.rb
@@ -2,6 +2,6 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
+# Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '74a55104ef4878594462d4c34949ccbcec15a552163e5d13bd8eb492be93c9da7da5913a56018b8f0ccf831288037fe2b2e276b9a059dc305fd2c453be794eab';
+SporkWeb::Application.config.secret_token = 'ee45a0ff7e8793077b3206f0bff3e4fc344bbe27fe8c0cc7e88eb3475c67dbc24062e20a5416da1b130853da8a334f51082c161a8a0ae18b1556e882b4f8d940'
