6c142c0ab3090a415edee79c09b6fbbb7e7a4b26
blake <blake.rego@gmail.com>
2012-08-05 18:35:22 -0400

    adding try catch block for authentication controller.

--- sporkWeb/app/controllers/authenticate_controller.rb
+++ sporkWeb/app/controllers/authenticate_controller.rb
@@ -1,20 +1,26 @@
 require 'open-uri'
+require 'foursquare'
 
 class AuthenticateController < ApplicationController
 
 	def index
-		clientId = '00WU15WUYS1QBU0LW0ZKXRUYUS0PJKRCLLI5SMECYELN25BX'
-		clientSecret = '2CYYKMTA045GUROSNCIOJOKR4SUIQWOYILVLNZGCXR1AKKND'
-		redirectUri = 'http://localhost:3000/authenticate/'
-		access_token_uri = 'https://foursquare.com/oauth2/access_token?client_id=' + 
-		  clientId + '&client_secret=' + 
-		  clientSecret + '&grant_type=authorization_code&redirect_uri=' + redirectUri + '&code='+
-		  params[:code]
+		begin
+			clientId = '00WU15WUYS1QBU0LW0ZKXRUYUS0PJKRCLLI5SMECYELN25BX'
+			clientSecret = '2CYYKMTA045GUROSNCIOJOKR4SUIQWOYILVLNZGCXR1AKKND'
+			redirectUri = 'http://localhost:3000/authenticate/'
+			access_token_uri = 'https://foursquare.com/oauth2/access_token?client_id=' + 
+			  clientId + '&client_secret=' + 
+			  clientSecret + '&grant_type=authorization_code&redirect_uri=' + redirectUri + '&code='+
+			  params[:code]
 
-	  	getresult = open(access_token_uri, "UserAgent" => "Ruby-OpenURI").read
-		output = {'foo' => 'bar', 'code' => params[:code], 'accessToken' => JSON.parse(getresult)['access_token']}.to_json
+	  		getresult = open(access_token_uri, "UserAgent" => "Ruby-OpenURI").read
+	  		accessToken = JSON.parse(getresult)['access_token']
+			output = {'foo' => 'bar', 'code' => params[:code], 'accessToken' => accessToken}.to_json
+			venue = Foursquare::Venue.new(accessToken)
+			rawMovingTargets = venue.search({:ll => "40.7619,-73.9763", :categoryId => "4f2a23984b9023bd5841ed2c"})
+		rescue 
+			output = {'foo' => 'bar', 'code' => params[:code], 'accessToken' => 'for test'}.to_json
+		end
 		render :json => output
 	end
-
-
 end
