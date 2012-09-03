c244c63f0bf9cc9e9b4762770b979f81add3e3e4
blake <blake.rego@gmail.com>
2012-09-03 16:42:42 -0400

    inspecting the results by adding server side print statements.

--- sporkWeb/app/controllers/authenticate_controller.rb
+++ sporkWeb/app/controllers/authenticate_controller.rb
@@ -4,34 +4,49 @@ require 'foursquare'
 class AuthenticateController < ApplicationController
 
 	def index
-		begin
-			puts 'in the index method of the authenticate controller'
-			clientId = '00WU15WUYS1QBU0LW0ZKXRUYUS0PJKRCLLI5SMECYELN25BX'
-			clientSecret = '2CYYKMTA045GUROSNCIOJOKR4SUIQWOYILVLNZGCXR1AKKND'
-			redirectUri = 'http://localhost:3000/authenticate/'
-			access_token_uri = 'https://foursquare.com/oauth2/access_token?client_id=' + 
-			  clientId + '&client_secret=' + 
-			  clientSecret + '&grant_type=authorization_code&redirect_uri=' + redirectUri + '&code='+
-			  params[:code]
+		puts 'in the index method of the authenticate controller.'
+		accessToken = getAccessToken(params)
+		puts 'access token uri: ' + accessToken
 
-	  		getresult = open(access_token_uri, "UserAgent" => "Ruby-OpenURI").read
-	  		accessToken = JSON.parse(getresult)['access_token']
-			
-			venue = Foursquare::Venue.new(accessToken)
-			rawMovingTargets = venue.search({:ll => "40.7619,-73.9763", :categoryId => "4f2a23984b9023bd5841ed2c"})
+		venue = Foursquare::Venue.new(accessToken)
+		
+		#### This is for all moving targets -- 4f2a23984b9023bd5841ed2c
+		rawMovingTargets = venue.search({:ll => "40.7619,-73.9763", :categoryId => "4f2a23984b9023bd5841ed2c"})
 
-			output = {'foo' => 'bar', 'code' => params[:code], 'accessToken' => accessToken, 
-				'rawMovingTargets'=> rawMovingTargets}.to_json
+		filtered = []
 
+		results = rawMovingTargets["response"]["venues"]
+		results.each do |venue|
+			puts 'name: ' + venue["name"]
+			puts 'categories: '
+			categories = venue["categories"]
 
-		rescue 
-			output = {'foo' => 'bar', 'code' => params[:code], 'accessToken' => 'for test'}.to_json
+			categories.each do |category|
+				categoryName = category['name'].to_s
+				puts 'category name: ' + categoryName
+				if categoryName.include? 'food'
+					puts '**********************************************************'
+					filtered.append(venue)
+				end
+			end
+
+			puts 'stats: '
+			#stats = venue["stats"]
+			#stats.each do |stat|
+			#	puts 'checkinsCount: ' + stat["checkinsCount"]
+			#	puts 'usersCount: ' + stat["usersCount"]
+			#	puts 'tipCount: ' + stat["tipCount"]
+			#end
+			puts ''
 		end
-		render :json => rawMovingTargets["notifications"]
+
+		render :json => filtered
 	end
 
-	def getAccessCode
+	def getAccessToken(params)
+		accessToken = ''
 		begin
+			puts 'in the getAccessToken method of the authenticate controller'
 			clientId = '00WU15WUYS1QBU0LW0ZKXRUYUS0PJKRCLLI5SMECYELN25BX'
 			clientSecret = '2CYYKMTA045GUROSNCIOJOKR4SUIQWOYILVLNZGCXR1AKKND'
 			redirectUri = 'http://localhost:3000/authenticate/'
@@ -42,15 +57,9 @@ class AuthenticateController < ApplicationController
 
 	  		getresult = open(access_token_uri, "UserAgent" => "Ruby-OpenURI").read
 	  		accessToken = JSON.parse(getresult)['access_token']
-			
-			venue = Foursquare::Venue.new(accessToken)
-			rawMovingTargets = venue.search({:ll => "40.7619,-73.9763", :categoryId => "4f2a23984b9023bd5841ed2c"})
-
-			output = {'foo' => 'bar', 'code' => params[:code], 'accessToken' => accessToken, 
-				'rawMovingTargets'=> rawMovingTargets}.to_json
-		rescue 
-			output = {'foo' => 'bar', 'code' => params[:code], 'accessToken' => 'for test'}.to_json
-		end
-		render :json => output		
+  		rescue
+  			puts 'failed to retrieve access token.'
+  		end
+  		return accessToken
 	end
 end
