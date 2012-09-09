b3c31ce6a3d2f5342afca6bd1dea46ec98dbc131
blake <blake.rego@gmail.com>
2012-09-09 01:10:38 -0400

    now set up with google maps api.

--- sporkWeb/app/assets/javascripts/MapBind.js
+++ sporkWeb/app/assets/javascripts/MapBind.js
@@ -1,7 +1,6 @@
 
 
 function bindMap(accessToken){
-	alert(accessToken) ;
 
 	$.ajax({
 	  url: "http://localhost:3000/food_truck?accessToken=" + accessToken,
@@ -10,3 +9,21 @@ function bindMap(accessToken){
 		alert('ajax response returned.'); 
 	});
 }
+
+function initialize() {
+  var mapOptions = {
+    zoom: 11,
+    center: new google.maps.LatLng(40.7619,-73.9763),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+}
+
+function loadScript() {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyDkwduooUJwQbaXS3wdgbspCjBg6WRx5jw&sensor=true&callback=initialize";
+  document.body.appendChild(script);
+}
+
+window.onload = loadScript;
