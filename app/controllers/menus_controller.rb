55247943c26c4d63f778ce29e54fa73727a48e58
blake <blake.rego@gmail.com>
2012-10-20 13:14:00 -0400

    moving the myModal div into the menu div.

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -15,6 +15,7 @@ class MenusController < ApplicationController
   def show
     @menu = Menu.find(params[:id])
     @menuItems = Item.all
+    @item = Item.new
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @menu }
